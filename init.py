bed45685e2342458ce1177be3b32ec69482a1e6a
Brian <brianhungerman@gmail.com>
2019-05-28 22:51:09 -0700

    Cleaning code + linting

--- init.py
+++ init.py
@@ -4,41 +4,30 @@ import io
 import os
 import sys
 
-REPO_URLS = ['https://github.com/bitnaughts/csharp.interpreter',
-             'https://github.com/bitnaughts/bitnaughts']
+REPO_URLS = ['bitnaughts', 'csharp.interpreter']
 
 
 def getRepos():
-    if REPO_URLS:
-        # Look into Python environment independent way to achieve directory changes.
-        # Alternatively, if the user has any issues, have the init.py file placed inside of /bitnaughts/Assets/ and omit the directory change (Lines 15-17).
-        abs_path = os.path.abspath(sys.path[0])
-        d_name = os.path.dirname(abs_path + '/bitnaughts/Assets/')
-        os.chdir(d_name)
-        try:
-            for repo in REPO_URLS:
-                print("DOWNLOADING:" + " " + repo)
-                repo_zip_url = repo + "/archive/master.zip"
-                get_repo_zip = requests.get(repo_zip_url)
-                repo_zip_file = zipfile.ZipFile(
-                    io.BytesIO(get_repo_zip.content))
-                print("\tEXTRACTING " + repo + "....")
-                repo_zip_file.extractall()
-                print("\tDONE")
-            return True
-        except Exception as e:
-            print(e)
-            return False
-    else:
-        return "No repositories for download."
+    os.chdir(os.path.dirname(os.path.abspath(
+        sys.path[0]) + "/bitnaughts/Assets/"))
+    try:
+        for repo in REPO_URLS:
+            print("DOWNLOADING:\t" + repo)
+            repo_zip_file = zipfile.ZipFile(io.BytesIO(requests.get(
+                "https://github.com/bitnaughts/" + repo + "/archive/master.zip").content))
+            print("EXTRACTING:\t" + repo)
+            repo_zip_file.extractall()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 
 def main():
-    status = getRepos()
-    if status:
-        print("You're all set!")
+    if getRepos():
+        print("INITIALIZATION COMPLETE!")
     else:
-        print("Something went wrong.")
+        print("Something went wrong. Submit an issue on our Github repository!")
 
 
 if __name__ == "__main__":
