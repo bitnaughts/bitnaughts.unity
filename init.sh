05a06a5dc580e9e5450418108d1c1909255c8132
Brian Hungerman <brianhungerman@gmail.com>
2019-06-16 16:57:30 -0700

    Adding new module

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd Assets
 git clone https://github.com/bitnaughts/bitnaughts.git
+git clone https://github.com/bitnaughts/bitnaughts.world.git
 git clone https://github.com/bitnaughts/bitnaughts.ui.ux.git
 git clone https://github.com/bitnaughts/bitnaughts.assets.git
 git clone https://github.com/bitnaughts/csharp.interpreter.git
