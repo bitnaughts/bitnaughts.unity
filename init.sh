997842f31da86a794d2c02c9f20d2260374f5ee1
Brian <brianhungerman@gmail.com>
2019-10-30 03:47:58 -0700

    Removing/distributing DB Module

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 cd Assets
 git clone https://github.com/bitnaughts/bitnaughts.git
 git clone https://github.com/bitnaughts/bitnaughts.assets.git
-git clone https://github.com/bitnaughts/bitnaughts.db.git
 git clone https://github.com/bitnaughts/bitnaughts.multiplayer.git
 git clone https://github.com/bitnaughts/bitnaughts.world.git
 git clone https://github.com/bitnaughts/bitnaughts.ui.ux.git
