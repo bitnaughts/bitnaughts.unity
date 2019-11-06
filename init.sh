fb47281ec09b710d4a4c633fc7b51504a9e5bbf5
Brian <brianhungerman@gmail.com>
2019-11-05 22:48:39 -0800

    Adding unity.async dependency

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd Assets
+# BitNaughts Core Modules
 git clone https://github.com/bitnaughts/bitnaughts.git
 git clone https://github.com/bitnaughts/bitnaughts.assets.git
 git clone https://github.com/bitnaughts/bitnaughts.multiplayer.git
@@ -7,4 +8,5 @@ git clone https://github.com/bitnaughts/bitnaughts.world.git
 git clone https://github.com/bitnaughts/bitnaughts.ui.ux.git
 git clone https://github.com/bitnaughts/csharp.interpreter.git
 git clone https://github.com/bitnaughts/sql.interpreter.git
-
+# Async/Await Integration Library
+git clone https://github.com/bitnaughts/unity.async.git
