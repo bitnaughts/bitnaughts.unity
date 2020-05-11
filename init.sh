4224b336d87363ecb5fb79e10aba02d0b490ca5b
Denis Vashchenko <denisvashchenko@gmail.com>
2020-05-11 12:15:40 -0700

    something more like this

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd Assets
 # BitNaughts Core Modules
-git clone --submodule https://github.com/bitnaughts/bitnaughts.git
-git clone --submodule https://github.com/bitnaughts/bitnaughts.assets.git
-git clone --submodule https://github.com/bitnaughts/bitnaughts.multiplayer.git
-git clone --submodule https://github.com/bitnaughts/bitnaughts.world.git
-git clone --submodule https://github.com/bitnaughts/bitnaughts.ui.ux.git
-git clone --submodule https://github.com/bitnaughts/csharp.interpreter.git
-git clone --submodule https://github.com/bitnaughts/sql.interpreter.git
+git submodule add https://github.com/bitnaughts/bitnaughts.git
+git submodule add https://github.com/bitnaughts/bitnaughts.assets.git
+git submodule add https://github.com/bitnaughts/bitnaughts.multiplayer.git
+git submodule add https://github.com/bitnaughts/bitnaughts.world.git
+git submodule add https://github.com/bitnaughts/bitnaughts.ui.ux.git
+git submodule add https://github.com/bitnaughts/csharp.interpreter.git
+git submodule add https://github.com/bitnaughts/sql.interpreter.git
 # Async/Await Integration Library
 git clone https://github.com/bitnaughts/unity.async.git
