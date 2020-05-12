e7f71e8dd36a03ad6a242d2829b83704c064db01
Brian Hungerman <brianhungerman@gmail.com>
2020-05-11 23:34:05 -0700

    Up to date test with submodules

--- init.sh
+++ init.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 cd Assets
 # BitNaughts Core Modules
-git clone https://github.com/bitnaughts/bitnaughts.git
-git clone https://github.com/bitnaughts/bitnaughts.assets.git
-git clone https://github.com/bitnaughts/bitnaughts.multiplayer.git
-git clone https://github.com/bitnaughts/bitnaughts.world.git
-git clone https://github.com/bitnaughts/bitnaughts.ui.ux.git
-git clone https://github.com/bitnaughts/csharp.interpreter.git
-git clone https://github.com/bitnaughts/sql.interpreter.git
-# Async/Await Integration Library
-git clone https://github.com/bitnaughts/unity.async.git
+git submodule add https://github.com/bitnaughts/bitnaughts.git
+git submodule add https://github.com/bitnaughts/bitnaughts.assets.git
+git submodule add https://github.com/bitnaughts/bitnaughts.multiplayer.git
+git submodule add https://github.com/bitnaughts/bitnaughts.world.git
+git submodule add https://github.com/bitnaughts/bitnaughts.ui.ux.git
+git submodule add https://github.com/bitnaughts/csharp.interpreter.git
+git submodule add https://github.com/bitnaughts/sql.interpreter.git
