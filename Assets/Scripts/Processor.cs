210e52e042a61e169855a5dd8ad95c968a327f83
Brian Hungerman <brianhungerman@gmail.com>
2020-06-03 00:39:03 -0700

    Refining ProcessorController file structure

--- Assets/Scripts/Processor.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Threading.Tasks;
-using UnityEngine;
-
