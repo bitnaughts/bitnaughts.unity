c9052867e40c53fa8f60e42735485975ab59c215
Nick Tong <nicktong.001@gmail.com>
2020-06-09 17:10:00 -0400

    Added rotation and velocity methods in SensorController
    Velocity method needs more work

--- Assets/Scripts/Components/SensorController.cs
+++ Assets/Scripts/Components/SensorController.cs
@@ -11,7 +11,14 @@ public class SensorController : ComponentController {   //RangeFinder == 1D, Sca
     // public float max_step = 25f;
 
     private float distance = 0, distance_min = 0, distance_max = 999, scan_range = 0;
+    private Vector3 prevPosition;
+    private GameObject ship, shipRotator;
 
+    public void Start() {
+        ship = GameObject.Find("/Ship");
+        shipRotator = GameObject.Find("/Ship/Rotator");
+        prevPosition = ship.transform.position;
+    }
 
     public override float Action (float input) 
     {
@@ -28,8 +35,26 @@ public class SensorController : ComponentController {   //RangeFinder == 1D, Sca
             return distance_max;
         // return 0;
     }
+
+    public float GetRotation () { // Probably will be put into a new sensor component later
+        float rot = shipRotator.transform.eulerAngles.y; // Degrees clockwise from the negative z axis
+        return rot;
+    }
+
+    public Vector2 GetVelocity() {
+        // Without a rigidbody component, velocity is found from rate of change of distance
+        Vector3 deltaPosition = ship.transform.position - prevPosition;
+        prevPosition = ship.transform.position;
+        Vector3 vel = deltaPosition / Time.deltaTime;
+        return new Vector2(vel.x, vel.z) * -1; // (-left|+right, +forward|-backward) world space
+    }
+
     public override string ToString()
-    {
+    {   
+        
+        // Debug.Log(GetRotation());
+        // Debug.Log(GetVelocity());
+
           //Move to "audio sensor" LOL, could actually be legit useful if actions cause sounds, local audio listener picks it up and can target it? sound-tracking missile...
         // string output = "";
         // float[] spectrum = new float[64];
